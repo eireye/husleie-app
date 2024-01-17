b019a7ef242c39697d054970e1c22e364629364b
eireye <oeye@protonmail.com>
2024-01-17 09:09:19 +0100

    Fikset komma problemet

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,27 @@ fn main() -> Result<(), slint::PlatformError> {
 
     ui.on_utregning_belop(move |total, rente, avdrag, strom1, strom2, strom3| {
         let ui = ui_handle.unwrap();
-        let total_num = total.trim().parse::<f64>().unwrap_or(0.0);
-        let rente_num = rente.trim().parse::<f64>().unwrap_or(0.0);
-        let avdrag_num = avdrag.trim().parse::<f64>().unwrap_or(0.0);
-        let strom1_num = strom1.trim().parse::<f64>().unwrap_or(0.0);
-        let strom2_num = strom2.trim().parse::<f64>().unwrap_or(0.0);
-        let strom3_num = strom3.trim().parse::<f64>().unwrap_or(0.0);
 
-        let rente2_num: f64 = rente_num / 2.0;
-        let mari_andel: f64 = total_num - rente_num;
-        let totalen_andeler: f64 = avdrag_num + mari_andel;
-        let andel_eirik: f64 = avdrag_num / totalen_andeler;
-        let sum_eirik: f64 = mari_andel * andel_eirik + rente2_num + strom1_num + strom2_num + strom3_num;
+        let total_num = parse_to_f64(&total);
+        let rente_num = parse_to_f64(&rente);
+        let avdrag_num = parse_to_f64(&avdrag);
+        let strom1_num = parse_to_f64(&strom1);
+        let strom2_num = parse_to_f64(&strom2);
+        let strom3_num = parse_to_f64(&strom3);
+
+        let rente2_num = rente_num / 2.0;
+        let mari_andel = total_num - rente_num;
+        let totalen_andeler = avdrag_num + mari_andel;
+        let andel_eirik = avdrag_num / totalen_andeler;
+        let sum_eirik = mari_andel * andel_eirik + rente2_num + strom1_num + strom2_num + strom3_num;
+
         let result = format!("Eirik skal betale: {:.2} ", sum_eirik);
-
         ui.set_results(result.into());
     });
 
     ui.run()
+}
+
+fn parse_to_f64(input: &str) -> f64 {
+    input.trim().replace(',', ".").parse::<f64>().unwrap_or(0.0)
 }
